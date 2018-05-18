ac706c45ecc5675590d69a301ee5580c79da1231
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-18 15:19:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,9 +52,9 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT count(*)
+      SELECT *
       FROM students
-      WHERE grade <  "12"
+      WHERE grade <=  "11"
       LIMIT 1
     SQL
 
