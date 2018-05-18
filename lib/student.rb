c96618b2d74a23e157619f04b766b9129e4e7b7b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-18 15:12:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,17 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade <  "9th"
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
 
   def save
     sql = <<-SQL
